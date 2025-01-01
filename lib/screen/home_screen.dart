7a2a6dbd5aaa7e38f73a0482dce12c59b34317d4
woogyeom <woogyeom99@gmail.com>
2025-01-01 01:45:24 -0500

    added Backward, Forward buttons

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   WebViewController webViewController = WebViewController()
-    ..loadRequest(Uri.parse('https://www.naver.com'))
+    ..loadRequest(Uri.parse('https://www.google.com'))
     ..setJavaScriptMode(JavaScriptMode.unrestricted);
 
   HomeScreen({Key? key}) : super(key: key);
@@ -15,16 +15,35 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.blue,
         title: Text('Home Screen', style: TextStyle(color: Colors.white)),
         centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            webViewController.goBack();
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
         actions: [
           IconButton(
-              onPressed: () {
-                webViewController
-                    .loadRequest(Uri.parse('https://www.naver.com'));
-              },
-              icon: Icon(
-                Icons.home,
-                color: Colors.white,
-              ))
+            onPressed: () {
+              webViewController.goForward();
+            },
+            icon: Icon(
+              Icons.arrow_forward,
+              color: Colors.white,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              webViewController
+                  .loadRequest(Uri.parse('https://www.google.com'));
+            },
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+          ),
         ],
       ),
       body: WebViewWidget(
